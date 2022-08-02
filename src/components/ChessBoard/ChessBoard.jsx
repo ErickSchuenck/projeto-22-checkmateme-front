4aa25a5ad26b001ca3fd79d05f7cbf547925fe08
ErickSchuenck <esfmokdeci@gmail.com>
2022-08-02 18:41:18 -0300

    comp board now colored

--- src/components/ChessBoard/ChessBoard.jsx
+++ src/components/ChessBoard/ChessBoard.jsx
@@ -2,19 +2,33 @@ import styled from 'styled-components';
 
 export default function ChessBoard() {
 
+  
+  const YAxis = ['8','7','6','5','4','3','2','1']
   const XAxis = ['a','b','c','d','e','f','g','h']
-  const YAxis = ['1','2','3','4','5','6','7','8']
   let board = []
 
-  for (let x =0; x < XAxis.length; x++){
-    for (let y =0; y < XAxis.length; y++){
-      board.push(
-        <div className='square'>
+  for (let y =0; y < YAxis.length; y++){
+    for (let x =0; x < XAxis.length; x++){
+      const coordinateNumber = y + x;
+      if (coordinateNumber % 2 === 0){
+        board.push(
+        <div className='square blackSquare'>
           <h1>
             {XAxis[x]}{YAxis[y]}
           </h1>
         </div>
       )
+      }
+
+      if (coordinateNumber % 2 !== 0){
+        board.push(
+        <div className='square whiteSquare'>
+          <h1>
+            {XAxis[x]}{YAxis[y]}
+          </h1>
+        </div>
+      )
+      }
     }
   };
 
@@ -30,11 +44,9 @@ const Container = styled.div`
   grid-template-columns: repeat(8, 60px);
   grid-template-rows: repeat(8, 60px);
   color: #854a4a;
-  background-color: #EEDAB6;
   max-width:480px;
   margin: 40px; // FIXME
-  .square{
-    background-color: #C7A37B;
+  .square{ 
     width: 58px;
     height: 58px;
     h1{
@@ -44,4 +56,11 @@ const Container = styled.div`
       font-size: 20px;
     }
   }
+  .blackSquare{
+    background-color: #C7A37B;  
+  }
+  .whiteSquare{
+    background-color: #EEDAB6;
+  }
+
 `
