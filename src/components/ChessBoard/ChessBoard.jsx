ba88a74a918bda92403aed9738af87a1360a5db0
ErickSchuenck <esfmokdeci@gmail.com>
2022-08-02 18:22:53 -0300

    starting to develop chessboard

--- src/components/ChessBoard/ChessBoard.jsx
+++ src/components/ChessBoard/ChessBoard.jsx
@@ -3,15 +3,17 @@ import styled from 'styled-components';
 export default function ChessBoard() {
 
   const XAxis = ['a','b','c','d','e','f','g','h']
-  const YAxis = [1,2,3,4,5,6,7,8]
+  const YAxis = ['1','2','3','4','5','6','7','8']
   let board = []
 
   for (let x =0; x < XAxis.length; x++){
     for (let y =0; y < XAxis.length; y++){
       board.push(
-        <span>
-          {XAxis[x]}{YAxis[y]}
-        </span>
+        <div className='square'>
+          <h1>
+            {XAxis[x]}{YAxis[y]}
+          </h1>
+        </div>
       )
     }
   };
@@ -24,9 +26,22 @@ export default function ChessBoard() {
 }
 
 const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 500px;
-  height: 500px;
+  display: grid;
+  grid-template-columns: repeat(8, 60px);
+  grid-template-rows: repeat(8, 60px);
+  color: #854a4a;
   background-color: #EEDAB6;
+  max-width:480px;
+  margin: 40px; // FIXME
+  .square{
+    background-color: #C7A37B;
+    width: 58px;
+    height: 58px;
+    h1{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 20px;
+    }
+  }
 `
