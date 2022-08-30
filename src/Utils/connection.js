b776992c0a1b059123ee410f1590cc9c018b3798
Gabriel d Agosto <gabriel.dagosto@engenharia.ufjf.br>
2022-08-30 19:53:57 -0300

    perf: change js-chess-engine to stockfish

--- src/Utils/connection.js
+++ src/Utils/connection.js
@@ -1,28 +1,37 @@
-import axios from "axios"
-import { convertNotationFromAppToApi } from "./convertNotation";
+import axios from 'axios';
+import { convertNotationFromAppToApi } from './convertNotation';
 
 export async function sendMoveToServer(previousX, previousY, newX, newY) {
-  const { from, to } = convertNotationFromAppToApi(previousX, previousY, newX, newY)
-  const body = { from, to }
+  const { from, to } = convertNotationFromAppToApi(
+    previousX,
+    previousY,
+    newX,
+    newY
+  );
+  const body = { move: `${from}${to}` };
   let computerMove;
   try {
-    await axios.post("http://localhost:5000/move", body)
-      .then((response) => { computerMove = response.data })
+    console.log(body);
+    await axios
+      .post('http://localhost:5000/move', body)
+      .then(response => {
+        computerMove = response.data;
+      })
       .catch(error => console.log(error));
   } catch (error) {
-    alert("Erro de servidor");
+    alert('Erro de servidor');
     console.log(error.response);
   }
-  return computerMove
+  return computerMove;
 }
 
 export async function startGame() {
   try {
-    await axios.post("http://localhost:5000/create")
+    await axios
+      .post('http://localhost:5000/create')
       .catch(error => console.log(error));
   } catch (error) {
-    alert("Erro de servidor");
+    alert('Erro de servidor');
     console.log(error.response);
   }
-  return
 }
--- src/Utils/convertNotation.js
+++ src/Utils/convertNotation.js
@@ -1,41 +1,38 @@
-
-
 export function convertNotationFromAppToApi(previousX, previousY, newX, newY) {
-  const XAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-  const YAxis = ['1', '2', '3', '4', '5', '6', '7', '8']
+  const XAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  const YAxis = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
-  return ({
+  return {
     from: `${XAxis[previousX]}${YAxis[previousY]}`,
     to: `${XAxis[newX]}${YAxis[newY]}`
-  })
+  };
 }
 
 export function convertNotationFromApiToApp(computerMove) {
-  let { from, to } = computerMove
-  const fromLetter = from[0]
-  const toLetter = to[0]
+  const fromLetter = computerMove[0];
+  const toLetter = computerMove[2];
 
-  const previousX = convertLetterIntoNumber(fromLetter)
-  const newX = convertLetterIntoNumber(toLetter)
+  const previousX = convertLetterIntoNumber(fromLetter);
+  const newX = convertLetterIntoNumber(toLetter);
 
-  const previousY = parseInt(from[1]) - 1
-  const newY = parseInt(to[1]) - 1
+  const previousY = parseInt(computerMove[1]) - 1;
+  const newY = parseInt(computerMove[3]) - 1;
 
-  from = { previousX, previousY }
-  to = { newX, newY }
+  const from = { previousX, previousY };
+  const to = { newX, newY };
 
-  return { from, to }
+  return { from, to };
 }
 
 function convertLetterIntoNumber(value) {
-  value = value.toLowerCase()
+  value = value.toLowerCase();
 
-  if (value === 'a') return 0
-  if (value === 'b') return 1
-  if (value === 'c') return 2
-  if (value === 'd') return 3
-  if (value === 'e') return 4
-  if (value === 'f') return 5
-  if (value === 'g') return 6
-  if (value === 'h') return 7
+  if (value === 'a') return 0;
+  if (value === 'b') return 1;
+  if (value === 'c') return 2;
+  if (value === 'd') return 3;
+  if (value === 'e') return 4;
+  if (value === 'f') return 5;
+  if (value === 'g') return 6;
+  if (value === 'h') return 7;
 }
